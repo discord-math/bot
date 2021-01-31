53934f6b8f6cdd54ec7973fbe76538f008b91e03
mniip <mniip@mniip.com>
2021-01-31 03:53:33 +0300

    Refactor plugins a little and document everything

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,3 +1,13 @@
+"""
+This module makes all modules in a specified namespace (plugins_namespace)
+behave slightly differently.
+
+We track whenever one plugin imports another, and keep a dependency graph
+around. We provide high level unload/reload functions that will unload/reload
+dependent plugins as well. We also provide a way for a plugin to hook a
+"finalizer" to be executed when the plugin is to be unloaded.
+"""
+
 import importlib.machinery
 import importlib.util
 import builtins
@@ -12,28 +22,80 @@ def is_plugin(name):
 deps = util.digraph.Digraph()
 import_stack = []
 
+def current_plugin():
+    """
+    In the lexical scope of the plugin, __name__ will refer to the plugin's
+    name.  This function can be used to get the name of the plugin in the
+    *dynamic* scope of its initialization. In other words functions outside a
+    plugin can get to know its name if they were called during plugin
+    initialization.
+    """
+    if not len(import_stack):
+        raise ValueError("not called during plugin initialization")
+    return import_stack[-1]
+
 def trace_import(name, globals=None, locals=None, fromlist=(), level=0):
-    last = import_stack[-1]
+    current = current_plugin()
     name_parts = name.split(".")
     for i in range(1, len(name_parts) + 1):
         parent = ".".join(name_parts[:i])
         if is_plugin(parent):
-            deps.add_edge(last, parent)
+            deps.add_edge(current, parent)
     return builtins.__import__(name, globals, locals, fromlist, level)
 
 trace_builtins = types.ModuleType(builtins.__name__)
 trace_builtins.__dict__.update(builtins.__dict__)
 trace_builtins.__import__ = trace_import
 
+finalizers = {}
+
+
+def finalizer(fin):
+    """
+    A decorator for registering a finalizer, which will be called during
+    unloading/reloading of a plugin. E.g.:
+
+        log = open("log", "w")
+        @plugins.finalizer
+        def close_log():
+            log.close()
+
+    If a module initialization fails to complete, the finalizers that managed
+    to register will be called.
+    """
+
+    current = current_plugin()
+    if current not in finalizers:
+        finalizers[current] = []
+    finalizers[current].append(fin)
+    return fin
+
+def finalize_module(name):
+    if name not in finalizers:
+        return
+    gen = finalizers[name].__iter__()
+    def cont_finalizers():
+        try:
+            for fin in gen:
+                fin()
+        except:
+            cont_finalizers()
+            raise
+        del finalizers[name]
+    cont_finalizers()
 
 class PluginLoader(importlib.machinery.SourceFileLoader):
     def exec_module(self, mod):
+        name = mod.__name__
         mod.__builtins__ = trace_builtins
-        import_stack.append(mod.__name__)
+        import_stack.append(name)
         try:
             importlib.machinery.SourceFileLoader.exec_module(self, mod)
         except:
-            deps.del_edges_from(mod.__name__)
+            try:
+                finalize_module(name)
+            finally:
+                deps.del_edges_from(name)
             raise
         finally:
             import_stack.pop()
@@ -54,34 +116,26 @@ for i in range(len(sys.meta_path)):
     if sys.meta_path[i] == importlib.machinery.PathFinder:
         sys.meta_path.insert(i, PluginFinder)
 
-finalizers = {}
-
-def finalizer(fin):
-    if not len(import_stack):
-        raise ValueError("not called during plugin initialization")
-    name = import_stack[-1]
-    if name not in finalizers:
-        finalizers[name] = []
-    finalizers[name].append(fin)
-    return fin
-
 def unsafe_unload(name):
+    """
+    Finalize and unload a single plugin. May break any plugins that depend on
+    it. All finalizers will be executed even if some raise exceptions, if there
+    were any they will all be reraised together.
+    """
     if not is_plugin(name):
         raise ValueError(name + " is not a plugin")
-    gen = finalizers[name].__iter__() if name in finalizers else ()
-    def cont_finalizers():
-        try:
-            for fin in gen:
-                fin()
-        except:
-            cont_finalizers()
-            raise
-        del finalizers[name]
-        del sys.modules[name]
+    try:
+        finalize_module(name)
+    finally:
         deps.del_edges_from(name)
-    cont_finalizers()
+        del sys.modules[name]
 
 def unload(name):
+    """
+    Finalize and unload a plugin and any plugins that (transitively) depend on
+    it. All finalizers will be executed even if some raise exceptions, if there
+    were any they will all be reraised together.
+    """
     gen = deps.subgraph_paths_to(name).topo_sort_fwd()
     def cont_unload():
         try:
@@ -94,7 +148,35 @@ def unload(name):
         unsafe_unload(name)
     cont_unload()
 
+def unsafe_reload(name):
+    """
+    Finalize and reload a single plugin. This will run the new plugin code over
+    the same module object, which may break any plugins that depend on it. All
+    finalizers will be executed even if some raise exceptions. If there were any
+    or if there was an exception during reinitialization, they will all be
+    reraised together. If plugin initialization raises an exception the plugin
+    remains loaded but may be in a half-updated state. Its finalizers aren't run
+    immediately. Returns the module object if successful.
+    """
+    if not is_plugin(name):
+        raise ValueError(name + " is not a plugin")
+    try:
+        finalize_module(name)
+    finally:
+        deps.del_edges_from(name)
+        ret = importlib.reload(sys.modules[name])
+    return ret
+
 def reload(name):
+    """
+    Finalize and reload a plugin and any plugins that (transitively) depend on
+    it. We try to run all finalizers in dependency order, and only load plugins
+    that were successfully unloaded, and whose dependencies have been
+    successfully reloaded. If a plugin fails to initialize, we run any
+    finalizers it managed to register, and the plugin is not loaded. Any
+    exceptions raised will be reraised together. Returns the module object of
+    the requested plugin if successful.
+    """
     reloads = deps.subgraph_paths_to(name)
     unload_success = set()
     reload_success = set()
--- util/digraph.py
+++ util/digraph.py
@@ -1,9 +1,13 @@
 class Digraph():
+    """A directed graph with no isolated vertices and no duplicate edges."""
+
     def __init__(self):
+        """Create an empty graph."""
         self.fwd = {}
         self.bck = {}
 
     def add_edge(self, x, y):
+        """Add an edge from x to y."""
         if x not in self.fwd:
             self.fwd[x] = set()
         self.fwd[x].add(y)
@@ -12,12 +16,18 @@ class Digraph():
         self.bck[y].add(x)
 
     def edges_to(self, x):
+        """Return a (read-only) set of edges into x."""
         return self.bck[x] if x in self.bck else set()
 
     def edges_from(self, x):
+        """Return a (read-only) set of edges from x."""
         return self.fwd[x] if x in self.fwd else set()
 
     def subgraph_paths_to(self, x):
+        """
+        Return an induced subgraph of exactly those vertices that can reach x
+        via a path.
+        """
         graph = Digraph()
         seen = set()
         def dfs(x):
@@ -32,6 +42,10 @@ class Digraph():
         return graph
 
     def topo_sort_fwd(self):
+        """
+        Iterate through vertices in such a way that whenever there is an edge
+        from x to y, x will come up earlier in iteration than y.
+        """
         seen = set()
         def dfs(x):
             if x in seen:
@@ -47,6 +61,10 @@ class Digraph():
             yield from dfs(x)
 
     def topo_sort_bck(self):
+        """
+        Iterate through vertices in such a way that whenever there is an edge
+        from x to y, x will come up later in iteration than y.
+        """
         seen = set()
         def dfs(x):
             if x in seen:
@@ -62,12 +80,18 @@ class Digraph():
             yield from dfs(x)
 
     def del_edges_from(self, x):
+        """
+        Delete all edges from x.
+        """
         if x in self.fwd:
             for y in self.fwd[x]:
                 self.bck[y].discard(x)
             del self.fwd[x]
 
     def del_edges_to(self, x):
+        """
+        Delete all edges into x.
+        """
         if x in self.bck:
             for y in self.bck[x]:
                 self.fwd[y].discard(x)
