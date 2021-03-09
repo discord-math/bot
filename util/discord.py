75bc8f17a4c351d0da14794852d9674600682e8d
mniip <mniip@mniip.com>
2021-03-09 07:16:12 +0300

    Add MessageById

--- util/discord.py
+++ util/discord.py
@@ -213,6 +213,12 @@ class ChannelById(discord.abc.Messageable):
     async def _get_channel(self):
         return self
 
+class MessageById:
+    __slots__ = "id"
+
+    def __init__(self, id):
+        self.id = id
+
 class RoleById:
     __slots__ = "id"
 
