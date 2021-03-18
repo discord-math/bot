20480003a1630b633908e18e12e4ee545f3f62a7
mniip <mniip@mniip.com>
2021-03-18 22:33:44 +0300

    Manage multiple repositories in `update`

--- plugins/update.py
+++ plugins/update.py
@@ -2,11 +2,18 @@ import asyncio
 import plugins.commands
 import plugins.privileges
 import util.discord
+import util.db.kv
+
+conf = util.db.kv.Config(__name__)
 
 @plugins.commands.command("update")
 @plugins.privileges.priv("admin")
 async def update_command(msg, args):
+    name = args.next_arg()
+    name = name.text if isinstance(name, plugins.commands.StringArg) else None
+
     git_pull = await asyncio.create_subprocess_exec("git", "pull", "--ff-only",
+        cwd=conf[name],
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.STDOUT)
     try:
