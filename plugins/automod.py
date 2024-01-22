8bb4d0a96159d1c05628d568e0bc021822b7c1fe
mniip <mniip@mniip.com>
2024-01-22 13:04:34 +0100

    Move automod away from kv

--- plugins/automod.py
+++ plugins/automod.py
@@ -1,14 +1,31 @@
 import asyncio
 from datetime import datetime, timedelta
+import enum
 import logging
 import re
-from typing import Awaitable, Dict, Iterable, List, Literal, Optional, Protocol, Set, Tuple, Union, cast, overload
+from typing import (
+    TYPE_CHECKING,
+    Dict,
+    Iterable,
+    List,
+    Literal,
+    Optional,
+    Set,
+    Union,
+    cast,
+)
 
 import discord
 from discord import AllowedMentions, Guild, Member, Message
 from discord.abc import Snowflake
 from discord.ext.commands import Greedy, group
 import discord.utils
+from sqlalchemy import ARRAY, TEXT, BigInteger, Enum, select
+from sqlalchemy.dialects.postgresql import INTERVAL
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+import sqlalchemy.orm
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.schema import CreateSchema
 
 from bot.acl import privileged
 from bot.client import client
@@ -30,78 +47,94 @@ from util.discord import (
     format,
     retry,
 )
-from util.frozen_list import FrozenList
-
-
-class AutomodConf(Awaitable[None], Protocol):
-    active: FrozenList[int]
-    index: int
-    exempt_roles: FrozenList[int]
-
-    @overload
-    def __getitem__(self, k: Tuple[int, Literal["keyword"]]) -> Optional[FrozenList[str]]:
-        ...
-
-    @overload
-    def __getitem__(self, k: Tuple[int, Literal["type"]]) -> Optional[Literal["substring", "word", "regex"]]:
-        ...
-
-    @overload
-    def __getitem__(
-        self, k: Tuple[int, Literal["action"]]
-    ) -> Optional[Literal["delete", "note", "mute", "kick", "ban"]]:
-        ...
-
-    @overload
-    def __getitem__(self, k: Tuple[int, Literal["duration"]]) -> Optional[float]:
-        ...
-
-    @overload
-    def __setitem__(self, k: Tuple[int, Literal["keyword"]], v: Optional[FrozenList[str]]) -> None:
-        ...
-
-    @overload
-    def __setitem__(self, k: Tuple[int, Literal["type"]], v: Optional[Literal["substring", "word", "regex"]]) -> None:
-        ...
-
-    @overload
-    def __setitem__(
-        self, k: Tuple[int, Literal["action"]], v: Optional[Literal["delete", "note", "mute", "kick", "ban"]]
-    ) -> None:
-        ...
-
-    @overload
-    def __setitem__(self, k: Tuple[int, Literal["duration"]], v: Optional[float]) -> None:
-        ...
 
 
 logger = logging.getLogger(__name__)
 
-conf: AutomodConf
+registry = sqlalchemy.orm.registry()
+sessionmaker = async_sessionmaker(util.db.engine, expire_on_commit=False)
 
 
-def to_regex(kind: Literal["substring", "word", "regex"], keyword: str) -> str:
-    if kind == "substring":
-        return re.escape(keyword)
-    elif kind == "word":
-        return r"\b{}\b".format(re.escape(keyword))
-    else:
-        return r"(?:{})".format(keyword)
+class MatchType(enum.Enum):
+    SUBSTRING = "substring"
+    WORD = "word"
+    REGEX = "regex"
 
 
+class ActionType(enum.Enum):
+    DELETE = "delete"
+    NOTE = "note"
+    MUTE = "mute"
+    KICK = "kick"
+    BAN = "ban"
+
+
+@registry.mapped
+class Rule:
+    __tablename__ = "rules"
+    __table_args__ = {"schema": "automod"}
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    keywords: Mapped[List[str]] = mapped_column(ARRAY(TEXT), nullable=False)
+    type: Mapped[MatchType] = mapped_column(Enum(MatchType, schema="automod"), nullable=False)
+    action: Mapped[Optional[ActionType]] = mapped_column(Enum(ActionType, schema="automod"))
+    action_duration: Mapped[Optional[timedelta]] = mapped_column(INTERVAL)
+
+    if TYPE_CHECKING:
+
+        def __init__(
+            self,
+            *,
+            keywords: List[str],
+            type: MatchType,
+            id: int = ...,
+            action: Optional[ActionType] = ...,
+            action_duration: Optional[timedelta] = ...,
+        ) -> None:
+            ...
+
+
+@registry.mapped
+class ExemptRole:
+    __tablename__ = "exempt_roles"
+    __table_args__ = {"schema": "automod"}
+
+    role_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+
+    if TYPE_CHECKING:
+
+        def __init__(self, *, role_id: int) -> None:
+            ...
+
+
+active_rules: Dict[int, Rule]
 regex: re.Pattern[str]
+exempt_roles: Set[int]
 
 
-def generate_regex() -> None:
-    global regex
-    parts = []
-    for i in conf.active:
-        if (keywords := conf[i, "keyword"]) is not None and (kind := conf[i, "type"]) is not None:
-            parts.append(r"(?P<_{}>{})".format(i, r"|".join(to_regex(kind, keyword) for keyword in keywords)))
-    if len(parts) > 0:
-        regex = re.compile("|".join(parts), re.I)
+def rule_to_regex(rule: Rule) -> str:
+    if rule.type == MatchType.SUBSTRING:
+        return r"|".join(re.escape(keyword) for keyword in rule.keywords)
+    elif rule.type == MatchType.WORD:
+        return r"|".join(r"\b" + re.escape(keyword) + r"\b" for keyword in rule.keywords)
     else:
-        regex = re.compile("(?!)")
+        return r"|".join(r"(?:" + keyword + r")" for keyword in rule.keywords)
+
+
+async def rehash_rules(session: AsyncSession) -> None:
+    global active_rules, regex, exempt_roles
+    stmt = select(Rule).where(Rule.action != None).order_by(Rule.id)
+    rules = (await session.execute(stmt)).scalars()
+    stmt = select(ExemptRole.role_id)
+    roles = (await session.execute(stmt)).scalars()
+
+    active_rules = {rule.id: rule for rule in rules}
+    exempt_roles = set(roles)
+
+    parts: List[str] = []
+    for rule in active_rules.values():
+        parts.append(r"(?P<_" + str(rule.id) + r">" + rule_to_regex(rule) + r")")
+    regex = re.compile("|".join(parts), re.I) if parts else re.compile("(?!)")
 
 
 def parse_note(text: Optional[str]) -> Dict[int, int]:
@@ -180,9 +213,11 @@ async def do_ban_user(guild: Guild, user: Snowflake, reason: str) -> None:
         logger.error(format("Could not ban user {!m} ({})", user, reason), exc_info=True)
 
 
-def fork_ban_user(guild: Guild, user: Snowflake, duration: Optional[float], reason: str) -> None:
+def fork_ban_user(guild: Guild, user: Snowflake, duration: Optional[timedelta], reason: str) -> None:
     if duration is not None:
-        reason = "Banned by {}: {} seconds, {}".format(client.user.id if client.user else None, int(duration), reason)
+        reason = "Banned by {}: {} seconds, {}".format(
+            client.user.id if client.user else None, int(duration.total_seconds()), reason
+        )
     asyncio.create_task(do_ban_user(guild, user, reason), name=format("Automod ban {!m}", user))
 
 
@@ -195,8 +230,8 @@ async def do_time_out(member: Member, until: datetime, reason: str) -> None:
         logger.error(format("Could not mute member {!m} ({})", member, reason), exc_info=True)
 
 
-def fork_time_out(member: Member, duration: float, reason: str) -> None:
-    until = discord.utils.utcnow() + timedelta(seconds=duration)
+def fork_time_out(member: Member, duration: timedelta, reason: str) -> None:
+    until = discord.utils.utcnow() + duration
     asyncio.create_task(do_time_out(member, until, reason), name=format("Automod mute {!m}", member))
 
 
@@ -204,7 +239,7 @@ def phish_match(msg: Message, text: str) -> None:
     assert msg.guild is not None
     logger.info("Message {} contains phishing: {}".format(msg.id, text))
     if isinstance(msg.author, Member):
-        if any(role.id in conf.exempt_roles for role in msg.author.roles):
+        if any(role.id in exempt_roles for role in msg.author.roles):
             return
         fork_ban_user(msg.guild, msg.author, None, "Automatic action: found phishing domain: {}".format(text))
     fork_delete_message(msg)
@@ -245,31 +280,31 @@ async def process_messages(msgs: Iterable[Message]) -> None:
                     continue
                 logger.info("Message {} matches pattern {}".format(msg.id, index))
                 if isinstance(msg.author, Member):
-                    if any(role.id in conf.exempt_roles for role in msg.author.roles):
+                    if any(role.id in exempt_roles for role in msg.author.roles):
                         continue
-                if (action := conf[index, "action"]) is not None:
+                if (rule := active_rules.get(index)) is not None:
                     reason = "Automatic action: message matches pattern {}".format(index)
-                    duration = conf[index, "duration"]
+                    duration = rule.action_duration
 
-                    if action == "delete":
+                    if rule.action == ActionType.DELETE:
                         fork_delete_message(msg)
 
-                    elif action == "note":
+                    elif rule.action == ActionType.NOTE:
                         fork_delete_message(msg)
                         fork_create_automod_note(msg.author.id, index)
 
-                    elif action == "mute":
+                    elif rule.action == ActionType.MUTE:
                         fork_delete_message(msg)
                         if isinstance(msg.author, Member):
                             if duration is None:
-                                duration = 86400 * 28
+                                duration = timedelta(days=28)
                             fork_time_out(msg.author, duration, reason)
 
-                    elif action == "kick":
+                    elif rule.action == ActionType.KICK:
                         fork_delete_message(msg)
                         fork_kick_user(msg.guild, msg.author, reason)
 
-                    elif action == "ban":
+                    elif rule.action == ActionType.BAN:
                         fork_delete_message(msg)
                         fork_ban_user(msg.guild, msg.author, duration, reason)
         except:
@@ -278,20 +313,49 @@ async def process_messages(msgs: Iterable[Message]) -> None:
 
 @plugins.init
 async def init() -> None:
-    global conf
-    conf = cast(AutomodConf, await util.db.kv.load(__name__))
-    if conf.index is None:
-        conf.index = 1
-    if conf.active is None:
-        conf.active = FrozenList()
-    if conf.exempt_roles is None:
-        conf.exempt_roles = FrozenList()
-    for i in range(conf.index):
-        if isinstance(keyword := conf[i, "keyword"], str):
-            conf[i, "keyword"] = FrozenList((keyword,))
+    await util.db.init(util.db.get_ddl(CreateSchema("automod"), registry.metadata.create_all))
 
-    await conf
-    generate_regex()
+    async with sessionmaker() as session:
+        conf = await util.db.kv.load(__name__)
+
+        if conf.index is not None:
+            for i in range(cast(int, conf.index)):
+                if conf[i, "keyword"] is None or conf[i, "type"] is None:
+                    continue
+
+                type = MatchType(conf[i, "type"])
+                if i not in cast(List[int], conf.active):
+                    action = None
+                else:
+                    action = ActionType(conf[i, "action"])
+                if (seconds := cast(Optional[float], conf[i, "duration"])) is not None:
+                    action_duration = timedelta(seconds=seconds)
+                else:
+                    action_duration = None
+                session.add(
+                    Rule(
+                        id=i,
+                        keywords=cast(List[str], conf[i, "keyword"]),
+                        type=type,
+                        action=action,
+                        action_duration=action_duration,
+                    )
+                )
+            for role_id in cast(List[int], conf.exempt_roles):
+                session.add(ExemptRole(role_id=role_id))
+            await session.commit()
+            conf.index = None
+            await conf
+
+        if conf.exempt_roles is not None:
+            for role_id in cast(List[int], conf.exempt_roles):
+                session.add(ExemptRole(role_id=role_id))
+            await session.commit()
+            conf.exempt_roles = None
+            await conf
+
+        await rehash_rules(session)
+
     await bot.message_tracker.subscribe(__name__, None, process_messages, missing=True, retroactive=False)
 
     async def unsubscribe() -> None:
@@ -314,7 +378,8 @@ async def automod_command(ctx: Context) -> None:
 async def automod_exempt(ctx: Context) -> None:
     """Manage roles exempt from automod."""
     output = []
-    for id in conf.exempt_roles:
+    for id in exempt_roles:
+        output.append("{!M}")
         role = discord.utils.find(lambda r: r.id == id, ctx.guild.roles if ctx.guild is not None else ())
         if role is not None:
             output.append(format("{!M}({!i} {!i})", role, role.name, role.id))
@@ -327,22 +392,22 @@ async def automod_exempt(ctx: Context) -> None:
 @privileged
 async def automod_exempt_add(ctx: Context, role: PartialRoleConverter) -> None:
     """Make a role exempt from automod."""
-    roles = set(conf.exempt_roles)
-    roles.add(role.id)
-    conf.exempt_roles = FrozenList(roles)
-    await conf
-    await ctx.send(format("{!M} is now exempt from automod", role), allowed_mentions=AllowedMentions.none())
+    async with sessionmaker() as session:
+        session.add(ExemptRole(role_id=role.id))
+        await session.commit()
+        await rehash_rules(session)
+        await ctx.send(format("{!M} is now exempt from automod", role), allowed_mentions=AllowedMentions.none())
 
 
 @automod_exempt.command("remove")
 @privileged
 async def automod_exempt_remove(ctx: Context, role: PartialRoleConverter) -> None:
     """Make a role not exempt from automod."""
-    roles = set(conf.exempt_roles)
-    roles.discard(role.id)
-    conf.exempt_roles = FrozenList(roles)
-    await conf
-    await ctx.send(format("{!M} is no longer exempt from automod", role), allowed_mentions=AllowedMentions.none())
+    async with sessionmaker() as session:
+        await session.delete(await session.get(ExemptRole, role.id))
+        await session.commit()
+        await rehash_rules(session)
+        await ctx.send(format("{!M} is no longer exempt from automod", role), allowed_mentions=AllowedMentions.none())
 
 
 @automod_command.command("list")
@@ -350,23 +415,24 @@ async def automod_exempt_remove(ctx: Context, role: PartialRoleConverter) -> Non
 async def automod_list(ctx: Context) -> None:
     """List all automod patterns (CW)."""
     items = [PlainItem("**Automod patterns**:\n")]
-    for i in conf.active:
-        if (
-            (keywords := conf[i, "keyword"]) is not None
-            and (kind := conf[i, "type"]) is not None
-            and (action := conf[i, "action"]) is not None
-        ):
-            if action in ["mute", "ban"]:
-                duration = " for {} seconds".format(conf[i, "duration"]) if conf[i, "duration"] else " permanently"
-            else:
-                duration = ""
-            items.append(
-                PlainItem(
-                    "**{}**: {} {} -> {}{}\n".format(
-                        i, kind, ", ".join(format("||{!i}||", keyword) for keyword in keywords), action, duration
-                    )
+    for rule in active_rules.values():
+        if rule.action is None:
+            continue
+        if rule.action in [ActionType.MUTE, ActionType.BAN]:
+            duration = " for {} seconds".format(rule.action_duration) if rule.action_duration else " permanently"
+        else:
+            duration = ""
+        items.append(
+            PlainItem(
+                "**{}**: {} {} -> {}{}\n".format(
+                    rule.id,
+                    rule.type.value,
+                    ", ".join(format("||{!i}||", keyword) for keyword in rule.keywords),
+                    rule.action.value,
+                    duration,
                 )
             )
+        )
 
     for content, _ in chunk_messages(items):
         await ctx.send(content)
@@ -387,11 +453,14 @@ async def automod_add(
     ctx.send = ctx.channel.send  # type: ignore # Undoing the effect of cleanup
     if len(patterns) == 0:
         raise InvocationError("Provide at least one pattern")
+
+    kind_enum = MatchType(kind)
+
     keywords: List[str] = []
     for pattern in patterns:
         if isinstance(pattern, (CodeBlock, Inline)):
             pattern = pattern.text
-        if kind == "regex":
+        if kind_enum == MatchType.REGEX:
             try:
                 regex = re.compile(pattern)
             except Exception as exc:
@@ -403,19 +472,17 @@ async def automod_add(
                 raise UserError("The pattern is empty, that's probably not good")
         keywords.append(pattern)
 
-    for i in range(conf.index):
-        if conf[i, "keyword"] == keywords and conf[i, "type"] == kind and conf[i, "action"] == None:
-            break
-    else:
-        i = conf.index
-        conf.index += 1
-        conf[i, "keyword"] = FrozenList(keywords)
-        conf[i, "type"] = kind
-        conf[i, "action"] = None
-        await conf
+    async with sessionmaker() as session:
+        stmt = select(Rule).where(Rule.keywords == keywords, Rule.type == kind_enum, Rule.action == None).limit(1)
+        if not (rule := (await session.execute(stmt)).scalar()):
+            rule = Rule(keywords=keywords, type=kind_enum, action=None)
+            session.add(rule)
+            await session.commit()
+            await rehash_rules(session)
+
     await ctx.send(
         "Added {} as pattern **{}** with no action".format(
-            ", ".join(format("||{!i}||", keyword) for keyword in keywords), i
+            ", ".join(format("||{!i}||", keyword) for keyword in keywords), rule.id
         )
     )
 
@@ -424,19 +491,15 @@ async def automod_add(
 @privileged
 async def automod_remove(ctx: Context, number: int) -> None:
     """Remove an automod pattern by ID."""
-    keywords = conf[number, "keyword"]
-    kind = conf[number, "type"]
-    if keywords is not None and kind is not None:
-        conf[number, "action"] = None
-    active = set(conf.active)
-    active.discard(number)
-    conf.active = FrozenList(active)
-    await conf
-    generate_regex()
-    if keywords is not None and kind is not None:
-        await ctx.send("Removed {} {}".format(kind, ", ".join(format("||{!i}||", keyword) for keyword in keywords)))
-    else:
-        await ctx.send("No such pattern")
+    async with sessionmaker() as session:
+        if not (rule := await session.get(Rule, number)):
+            raise UserError("No such pattern")
+        rule.action = None
+        await session.commit()
+        await rehash_rules(session)
+        await ctx.send(
+            "Removed {} {}".format(rule.type, ", ".join(format("||{!i}||", keyword) for keyword in rule.keywords))
+        )
 
 
 @automod_command.command("action")
@@ -448,13 +511,11 @@ async def automod_action(
     duration: Optional[DurationConverter],
 ) -> None:
     """Assign an action to an automod pattern. (All actions imply deletion)."""
-    if conf[number, "keyword"] is None or conf[number, "type"] is None:
-        raise UserError("No such pattern")
-    conf[number, "action"] = action
-    conf[number, "duration"] = None if duration is None else duration.total_seconds()
-    active = set(conf.active)
-    active.add(number)
-    conf.active = FrozenList(active)
-    await conf
-    generate_regex()
-    await ctx.send("\u2705")
+    async with sessionmaker() as session:
+        if not (rule := await session.get(Rule, number)):
+            raise UserError("No such pattern")
+        rule.action = ActionType(action)
+        rule.action_duration = duration
+        await session.commit()
+        await rehash_rules(session)
+        await ctx.send("\u2705")
