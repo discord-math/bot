3a7555647f7a6008e9b6c8e14d77b2e18a2847fb
Hayley <wigglyworm91@yahoo.com>
2025-10-13 03:55:24 +1100

    removed rate limiter from animals command, added slightly more useful config, still broken

--- plugins/animals.py
+++ plugins/animals.py
@@ -1,7 +1,6 @@
 
 
 import abc
-from collections import defaultdict
 from dataclasses import dataclass
 import logging
 from typing import Any, Literal, Optional
@@ -13,85 +12,60 @@ import discord
 from bot.commands import Context, plugin_command
 from discord.ext.commands import command
 import aiohttp
-import datetime as dt
 
+from sqlalchemy import TEXT
+from sqlalchemy.orm import Mapped, mapped_column
+import sqlalchemy.orm
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+import plugins
 from util.discord import UserError
+import util.db
 
 CAT_API_ROOT = 'https://api.thecatapi.com/v1/images/search'
 DOG_API_ROOT = 'https://api.thedogapi.com/v1/images/search'
 TIMEOUT = 10  # seconds
 
-def with_suffix(obj: dict[str, Any], *path: str, suffix: str) -> Optional[str]:
-    val = obj
-    try:
-        for p in path:
-            val = val[p]
-    except (KeyError, TypeError):
-        return None
-    if val is not None:
-        return f'{val}{suffix}'
-    return None
+logger = logging.getLogger(__name__)
 
-class RateLimiter(abc.ABC):
+cat_api: Optional['AnimalApi'] = None
+dog_api: Optional['AnimalApi'] = None
+
+http = aiohttp.ClientSession()
+plugins.finalizer(http.close)
+
+registry = sqlalchemy.orm.registry()
+sessionmaker = async_sessionmaker(util.db.engine, expire_on_commit=False)
+
+@registry.mapped
+class GlobalConfig:
+    __tablename__ = "config"
+    __table_args__ = {"schema": "animals"}
+
+    id: Mapped[int] = mapped_column(primary_key=True, default=0)
+    cat_api_key: Mapped[str] = mapped_column(TEXT)
+    dog_api_key: Mapped[str] = mapped_column(TEXT)
+
+class AnimalRequest(abc.ABC):
     @abc.abstractmethod
-    def is_allowed(self, ctx: Context) -> tuple[bool, str]:
+    def to_dict(self) -> dict[str, Any]:
+        ''' Converts the request to a dictionary of query parameters '''
         pass
-
-class PerPersonRateLimiter(RateLimiter):
-    def __init__(self, calls: int, period: dt.timedelta):
-        self.calls = calls
-        self.period = period
-        self.users: dict[int, list[dt.datetime]] = defaultdict(list)
-
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}<{self.calls=}, {self.period=}>'
     
-    def __str__(self) -> str:
-        return f'{self.__class__.__name__}<{self.calls} calls per {self.period}>'
-    
-    def _is_allowed(self, user_id: int) -> tuple[bool, str]:
-        now = dt.datetime.now()
-        timestamps = self.users[user_id]
-        # Remove timestamps outside the period
-        while timestamps and now - timestamps[0] > self.period:
-            timestamps.pop(0)
-        if len(timestamps) < self.calls:
-            timestamps.append(now)
-            return True, ''
-        else:
-            next_time = timestamps[0] + self.period
-            return False, f'Rate limit exceeded: {self.calls} calls per {self.period}. Please try again <t:{int(next_time.timestamp())}:R>.'
-
-    def is_allowed(self, ctx: Context):
-        user_id = ctx.author.id
-        return self._is_allowed(user_id)
-    
-class GlobalRateLimiter(PerPersonRateLimiter):
-    def is_allowed(self, ctx: Context):
-        return self._is_allowed(0)  # Use a single user ID for global rate limiting
-
 @dataclass
-class CatRequest:
-    limit: Optional[int] = None
-    page: Optional[int] = None
-    order: Literal['ASC', 'DESC', 'RAND', None] = None
-    has_breeds: Optional[bool] = None
-    breed_ids: Optional[list[str]] = None
-    #sub_id: Optional[str]
+class CatRequest(AnimalRequest):
+    # https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR
 
-    def __post_init__(self):
-        try:
-            assert self.limit is None or 1 <= self.limit <= 100
-            assert self.page is None or 0 <= self.page
-        except AssertionError:
-            raise ValueError()
+    limit: Optional[int] = None # API defaults to 1
+    order: Literal['ASC', 'DESC', 'RAND', None] = None # API defaults to RAND
+    #page: Optional[int] = None # only relevant for ASC/DESC search
+    has_breeds: Optional[bool] = None # API defaults to all
+    breed_ids: Optional[list[str]] = None # API defaults to all
     
     def to_dict(self) -> dict[str, Any]:
         result = {}
         if self.limit is not None:
             result['limit'] = self.limit
-        if self.page is not None:
-            result['page'] = self.page
         if self.order is not None:
             result['order'] = self.order
         if self.has_breeds is not None:
@@ -101,14 +75,16 @@ class CatRequest:
         return result
 
 @dataclass
-class DogRequest:
+class DogRequest(AnimalRequest):
+    # https://docs.thedogapi.com/docs/examples/images
+
     size: Literal['full', 'med', 'small', 'thumb'] = 'small'
-    mime_types: Optional[list[Literal['jpg', 'png', 'gif']]] = None
+    mime_types: Optional[list[Literal['jpg', 'png', 'gif']]] = None # API defaults to all
     format: Literal['json', 'src'] = 'json'
-    order: Literal['ASC', 'DESC', 'RAND', None] = None
-    limit: Optional[int] = None
-    page: Optional[int] = None
-    has_breeds: Optional[bool] = None
+    order: Literal['ASC', 'DESC', 'RAND', None] = None # API defaults to RAND
+    limit: Optional[int] = None # API defaults to 1
+    #page: Optional[int] = None # only relevant for ASC/DESC search
+    has_breeds: Optional[bool] = None # API defaults to all
 
     def to_dict(self) -> dict[str, Any]:
         result = {}
@@ -118,13 +94,15 @@ class DogRequest:
         result['format'] = self.format
         result['order'] = self.order
         result['limit'] = self.limit
-        result['page'] = self.page
         if self.has_breeds is not None:
             result['has_breeds'] = int(self.has_breeds)
         return {k: v for k, v in result.items() if v is not None}
 
 @dataclass
 class AnimalResponse:
+    # We could define separate CatResponse and DogResponse classes,
+    #  but they're similar enough that it's simpler to just have one.
+
     id: str
     url: str
     width: int
@@ -133,27 +111,36 @@ class AnimalResponse:
     breeds: Optional[list[dict[str, Any]]] = None
 
     def get_weight(self) -> Optional[str]:
+        # The weight comes back in different formats depending on the API and the breed
         if self.breeds:
             breed = self.breeds[0]
-            return (
-                with_suffix(breed, 'weight', 'metric', suffix=' kg') or
-                with_suffix(breed, 'weight', 'imperial', suffix=' lbs') or
-                with_suffix(breed, 'weight', suffix='')
-            )
+            try:
+                return breed['weight']['metric'] + ' kg'
+            except KeyError:
+                pass
+
+            try:
+                return breed['weight']['imperial'] + ' lbs'
+            except KeyError:
+                pass
+
+            try:
+                return breed['weight'] + ''
+            except KeyError:
+                pass
         return None
 
     def get_life_span(self) -> Optional[str]:
         if self.breeds:
             breed = self.breeds[0]
-            lifespan = breed.get('life_span', None)
-            if not lifespan:
-                return None
-            if 'years' in lifespan:
+            try:
+                lifespan = breed['life_span']
+                if 'years' not in lifespan:
+                    lifespan += ' years'
                 return lifespan
-            else:
-                return f'{lifespan} years'
+            except KeyError:
+                pass
         return None
-
 
     def get_description(self) -> str:
         if self.breeds:
@@ -174,71 +161,38 @@ class AnimalResponse:
             return 'No breed information available.'
 
 class AnimalApi:
-    def __init__(self, api_root: str, api_key: Optional[str], rate_limiters: list[RateLimiter]) -> None:
+    # The actual API structure is identical, just with different parameters and API keys.
+
+    def __init__(self, api_root: str, api_key: str) -> None:
         self.api_root = api_root
         self.api_key = api_key
-        self.rate_limiters = rate_limiters
-    
-    def configure(self, api_key: str) -> None:
-        self.api_key = api_key
-    
-    def get_configuration(self) -> str:
-        out = ''
-        out += f'API Root: {self.api_root}\n'
-        out += f'API Key: {self.api_key}\n'
-        for limiter in self.rate_limiters:
-            out += f'Rate Limiter: {limiter}\n'
-        return out
 
-    async def fetch_random_animal(self, ctx: Context, params: dict[str, Any]) -> AnimalResponse:
-        logging.debug(f'Fetching random animal with params: {params}')
-        if not self.api_key:
-            raise UserError('Animal API is not configured.')
+    async def fetch_random_animal(self, req: Optional[AnimalRequest] = None) -> AnimalResponse:
+        params = req.to_dict() if req else {}
+        logger.debug(f'Fetching random animal with params: {params}')
         headers = {'x-api-key': self.api_key}
-        for limiter in self.rate_limiters:
-            allowed, message = limiter.is_allowed(ctx)
-            if not allowed:
-                raise UserError(message)
-        async with aiohttp.ClientSession() as session:
-            async with session.get(self.api_root, params=params, headers=headers, timeout=TIMEOUT) as resp:
-                data = await resp.json()
-                if not data:
-                    raise UserError('No animal found!')
-                return AnimalResponse(**data[0])
 
-class CatApi(AnimalApi):
-    def __init__(self, api_key: Optional[str], rate_limiters: list[RateLimiter]) -> None:
-        super().__init__(CAT_API_ROOT, api_key, rate_limiters)
+        async with http.get(
+            self.api_root,
+            params=params,
+            headers=headers,
+            timeout=TIMEOUT,
+        ) as resp:
+            data = await resp.json()
+            if not data:
+                raise UserError('No animal found!')
+            return AnimalResponse(**data[0])
 
-    async def fetch_random_cat(self, ctx: Context, req: CatRequest):
-        return await self.fetch_random_animal(ctx, req.to_dict())
-
-# cat api key should be set via the config command
-cat_api = CatApi('', [
-    # reasonable rate limits, I don't see a good way to configure these dynamically
-    PerPersonRateLimiter(calls=3, period=dt.timedelta(minutes=1)),
-    PerPersonRateLimiter(calls=20, period=dt.timedelta(hours=1)),
-    PerPersonRateLimiter(calls=100, period=dt.timedelta(days=1)),
-
-    GlobalRateLimiter(calls=10, period=dt.timedelta(minutes=1)),
-])
-
-class DogApi(AnimalApi):
-    def __init__(self, api_key: Optional[str], rate_limiters: list[RateLimiter]) -> None:
-        super().__init__(DOG_API_ROOT, api_key, rate_limiters)
-
-    async def fetch_random_dog(self, ctx: Context, req: DogRequest):
-        return await self.fetch_random_animal(ctx, req.to_dict())
-
-# dog api key should be set via the config command
-dog_api = DogApi('', [
-    # reasonable rate limits, I don't see a good way to configure these dynamically
-    PerPersonRateLimiter(calls=3, period=dt.timedelta(minutes=1)),
-    PerPersonRateLimiter(calls=20, period=dt.timedelta(hours=1)),
-    PerPersonRateLimiter(calls=100, period=dt.timedelta(days=1)),
-
-    GlobalRateLimiter(calls=10, period=dt.timedelta(minutes=1)),
-])
+def animal_to_embed(animal: AnimalResponse, title: str) -> discord.Embed:
+    embed = discord.Embed(
+        title=title,
+        url=animal.url,
+        color=discord.Color.random()
+    )
+    embed.set_image(url=animal.url)
+    footer = animal.get_description()
+    embed.set_footer(text=footer)
+    return embed
 
 @plugin_command
 @privileged
@@ -247,21 +201,11 @@ async def random_cat(ctx: Context) -> None:
     ''' Fetches and displays a random cat image '''
 
     async with ctx.typing():
-        # Prepare request params
-        req = CatRequest()
+        if not cat_api:
+            raise UserError('Cat API is not configured.')
 
-        cat = await cat_api.fetch_random_cat(ctx, req)
-        embed = discord.Embed(
-            title='Here is your random cat! 🐱',
-            url=cat.url,
-            color=discord.Color.random()
-        )
-        embed.set_image(url=cat.url)
-        footer = cat.get_description()
-        if footer:
-            footer += '\n'
-        footer += 'thecatapi.com'
-        embed.set_footer(text=footer)
+        cat = await cat_api.fetch_random_animal()
+        embed = animal_to_embed(cat, title='Here is your random cat! 🐱')
         await ctx.send(embed=embed)
 
 @plugin_command
@@ -271,31 +215,56 @@ async def random_dog(ctx: Context) -> None:
     ''' Fetches and displays a random dog image '''
 
     async with ctx.typing():
-        # Prepare request params
-        req = DogRequest()
-
-        dog = await dog_api.fetch_random_dog(ctx, req)
-        embed = discord.Embed(
-            title='Here is your random dog! 🐶',
-            url=dog.url,
-            color=discord.Color.random()
-        )
-        embed.set_image(url=dog.url)
-        footer = dog.get_description()
-        if footer:
-            footer += '\n'
-        footer += 'thedogapi.com'
-        embed.set_footer(text=footer)
+        if not dog_api:
+            raise UserError('Dog API is not configured.')
+        
+        dog = await dog_api.fetch_random_animal()
+        embed = animal_to_embed(dog, title='Here is your random dog! 🐶')
         await ctx.send(embed=embed)
 
-@plugin_config_command
-@privileged
-@group("animals", invoke_without_command=True)
-async def animals_config(ctx: Context, cat_api_key: Optional[str], dog_api_key: Optional[str]) -> None:
-    ''' Configures the animals plugin '''
-    if cat_api_key:
-        cat_api.configure(api_key=cat_api_key)
-    if dog_api_key:
-        dog_api.configure(api_key=dog_api_key)
+@plugins.init
+async def init() -> None:
+    global cat_api, dog_api
+    await util.db.init(util.db.get_ddl(registry.metadata.create_all))
 
-    await ctx.send(f'Animals plugin configuration:\n**Cat**\n{cat_api.get_configuration()}\n**Dog**\n{dog_api.get_configuration()}')
+    async with sessionmaker() as session:
+        conf = await session.get(GlobalConfig, 0)
+        if conf:
+            cat_api = AnimalApi(
+                CAT_API_ROOT,
+                conf.cat_api_key
+            )
+            dog_api = AnimalApi(
+                DOG_API_ROOT,
+                conf.dog_api_key
+            )
+        else:
+            raise UserError('No configuration found for animals plugin.')
+
+@plugin_config_command
+@group("animals")
+@privileged
+async def config(ctx: Context) -> None:
+    pass
+
+@config.command("cat_api_key")
+async def set_cat_api_key(ctx: Context, api_key: str) -> None:
+    global cat_api
+    cat_api = AnimalApi(CAT_API_ROOT, api_key)
+    async with sessionmaker() as session:
+        conf = await session.get(GlobalConfig, 0)
+        assert conf
+        conf.cat_api_key = api_key
+        await session.commit()
+    await ctx.send(f'\u2705')
+
+@config.command("dog_api_key")
+async def set_dog_api_key(ctx: Context, api_key: str) -> None:
+    global dog_api
+    dog_api = AnimalApi(DOG_API_ROOT, api_key)
+    async with sessionmaker() as session:
+        conf = await session.get(GlobalConfig, 0)
+        assert conf
+        conf.dog_api_key = api_key
+        await session.commit()
+    await ctx.send(f'\u2705')
